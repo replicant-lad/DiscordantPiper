f705d176f76751ecd1afa3553cd746fefcd89944
dldexter <85723331+dldexter@users.noreply.github.com>
2024-06-28 11:55:03 -0600

    Load handling for >9 queries.

--- responses.py
+++ responses.py
@@ -8,21 +8,24 @@ async def scryfall_query(message):
 
     card_queries = re.findall(r'\[\[([A-Za-z0-9_ ,-:/]+)\]\]', message.content)
 
+    if len(card_queries) >= 10:
+        await message.channel.send(f'This bot only allows for up to nine queries to keep response time high and to respect Scryfall API standards. Try again.')
 
-    for card in card_queries:
-        print(card)
-        card_get = loads(get(f"https://api.scryfall.com/cards/search?q={card}").text)
-        try:
-            card_images.append(card_get['data'][0]['image_uris']['png'])
-        except Exception as e:
-            print("Attempted to query a card that has no results, ignoring...")
-
-    cards = ('\n').join(card_images)
-
-    if len(card_queries) == len(card_images):
-        mismatch = ''
     else:
-        mismatch = f'Detected {len(card_queries)} card queries, but Scryfall only returned {len(card_images)}: \n'
+        for card in card_queries:
+            print(card)
+            card_get = loads(get(f"https://api.scryfall.com/cards/search?q={card}").text)
+            try:
+                card_images.append(card_get['data'][0]['image_uris']['png'])
+            except Exception as e:
+                print("Attempted to query a card that has no results, ignoring...")
+
+        cards = ('\n').join(card_images)
+
+        if len(card_queries) == len(card_images):
+            mismatch = ''
+        else:
+            mismatch = f'Detected {len(card_queries)} card queries, but Scryfall only returned {len(card_images)}: \n'
+            
         
-    
-    await message.channel.send(f'{mismatch} {cards}')
+        await message.channel.send(f'{mismatch} {cards}')
